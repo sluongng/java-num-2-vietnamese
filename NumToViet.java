2883cdbc1957fcd8910e149b121265541fd5c4a5
Son Luong Ngoc <son.luongngoc@lazada.com>
2019-05-21 23:39:10 +0800

    Some tests

---
+++ NumToViet.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class NumToViet {
+
+  // Algorithm section
+
+  public static String num2String(long num) {
+    return "";
+  }
+
+  // Test section
+
+  private static final Map<Long, String> testCase = new HashMap<Long, String>();
+  static {
+    testCase.put(10L, "mười");
+    testCase.put(100L, "một trăm");
+  }
+
+  public static void main(String[] args) {
+    Boolean result = testCase.entrySet().stream().map(entry -> {
+      String vietString = num2String(entry.getKey());
+
+      boolean comparision = entry.getValue().equals(vietString);
+      if (!comparision) {
+        System.out.println(String.format("Got result: |%s| expected |%s|", vietString, entry.getValue()));
+      }
+
+      return comparision;
+    }).reduce(Boolean.TRUE, (a, b) -> a.equals(b));
+
+    if (result) {
+      System.out.println("Finished testing!");
+    } else {
+      System.out.println("Some test(s) failed!");
+    }
+  }
+}
