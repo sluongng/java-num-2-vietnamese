80b0864206e63dd9100679ec4e23f80fd2f43ea0
Son Luong Ngoc <son.luongngoc@lazada.com>
2019-05-22 15:55:27 +0800

    Added some constants values

--- NumToViet.java
+++ NumToViet.java
@@ -1,8 +1,38 @@
 import java.util.HashMap;
-
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.List;
 
 public class NumToViet {
+
+  private static final List<String> digitsName = new ArrayList<String>() {
+    private static final long serialVersionUID = 1L;
+    {
+      add("không");
+      add("một");
+      add("hai");
+      add("ba");
+      add("bốn");
+      add("năm");
+      add("sáu");
+      add("bảy");
+      add("tám");
+      add("chín");
+    }
+  };
+
+  private static final List<String> thousandsName = new ArrayList<String>() {
+    private static final long serialVersionUID = 1L;
+    {
+      add("");
+      add("nghìn");
+      add("triệu");
+      add("tỷ");
+      add("nghìn tỷ");
+      add("triệu tỷ");
+      add("tỷ tỷ");
+    }
+  };
 
   // Algorithm section
 
@@ -16,19 +46,26 @@ public class NumToViet {
       return "âm " + num2String(-num);
     }
 
-    return "";
+    thousandsName.get(1);
+
+    return digitsName.get(1);
   }
 
   // Test section
 
-  private static final Map<Long, String> testCase = new HashMap<Long, String>();
-  static {
-    testCase.put(0L, "không");
-    testCase.put(1L, "một");
-    testCase.put(-1L, "âm một");
-    testCase.put(10L, "mười");
-    testCase.put(100L, "một trăm");
-  }
+  private static final Map<Long, String> testCase = new HashMap<Long, String>() {
+    private static final long serialVersionUID = 9999L;
+
+    {
+      put(0L, "không");
+      put(1L, "một");
+      put(-1L, "âm một");
+      put(10L, "mười");
+      put(100L, "một trăm");
+
+      put(1_000L, "một nghìn");
+    }
+  };
 
   public static void main(String[] args) {
     Boolean result = testCase.entrySet().stream().map(entry -> {
@@ -36,7 +73,9 @@ public class NumToViet {
 
       boolean comparision = entry.getValue().equals(vietString);
       if (!comparision) {
-        System.out.println(String.format("Got result: |%s| expected |%s|", vietString, entry.getValue()));
+        System.out.println(String.format("[Failed] result: |%s| expected |%s|", vietString, entry.getValue()));
+      } else {
+        System.out.println(String.format("[Passed] result: |%d| - |%s|", entry.getKey(), entry.getValue()));
       }
 
       return comparision;
